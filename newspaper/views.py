cc186ac1577ca12fb185c630a31146da71f8cb3f
Samin Shrestha <saminshrestha7@gmail.com>
2026-01-23 14:32:09 +0545

    did works on category and realted articles section

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from django.utils import timezone
 from datetime import timedelta
-from .models import Post    
+from .models import Post, Advertisement    
 
 # Create your views here.
 
-class HomeView(TemplateView):
+class SideBarMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+                      
+        context['popular_posts']=Post.objects.filter(
+            published_at__isnull=False, status="active"
+        ).order_by('-published_at')[:5]
+        
+        context['advertisement']=(
+            Advertisement.objects.all().order_by("-created_at").first()
+        )
+        
+        return context
+    
+class HomeView(SideBarMixin, TemplateView):
     template_name = "newsportal/home.html"
 
     def get_context_data(self, **kwargs):
@@ -25,20 +39,15 @@ class HomeView(TemplateView):
         context['trending_news']=Post.objects.filter(
             published_at__isnull=False, status="active"
         ).order_by('-views_count')[:4]   
-                      
-        context['popular_posts']=Post.objects.filter(
-            published_at__isnull=False, status="active"
-        ).order_by('-views_count')[:5]
         
         one_week_ago = timezone.now() - timedelta(days=7)
         context['weekly_top_posts']=Post.objects.filter(
             published_at__isnull=False, published_at__gte=one_week_ago, status="active"
         ).order_by('-published_at','-views_count')[:5]
-            
         
         return context
 
-class PostListView(ListView):
+class PostListView(SideBarMixin, ListView):
     model = Post
     template_name = "newsportal/list/list.html"
     context_object_name = "posts"
@@ -47,11 +56,45 @@ class PostListView(ListView):
     def get_queryset(self):
         return Post.objects.filter(published_at__isnull=False, status="active").order_by('-published_at')
     
+class PostDetailView(SideBarMixin, DetailView):
+    model = Post
+    template_name = "newsportal/detail/detail.html"
+    context_object_name = "post"
+
+    def get_queryset(self):
+        query = super().get_queryset()
+        query = query.filter(published_at__isnull=False, status="active")
+        return query
+    
+    
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-                      
-        context['popular_posts']=Post.objects.filter(
-            published_at__isnull=False, status="active"
-        ).order_by('-views_count')[:4]
+        context= super().get_context_data(**kwargs)
         
+        current_post = self.object
+        current_post.views_count += 1
+        current_post.save()
+        
+        context['related_articles']=(Post.objects.filter(
+            published_at__isnull=False,
+            status="active",
+            category=self.object.category
+            )
+            .exclude(id=self.object.id)
+            .order_by('-published_at','-views_count')[:2])  
         return context
+            
+            
+class PostByCategoryView(SideBarMixin, ListView):
+    model = Post
+    template_name = "newsportal/list/list.html"
+    context_object_name = "posts"
+    paginate_by = 1
+
+    def get_queryset(self):
+        query= super().get_queryset()
+        query= query.filter(
+            published_at__isnull=False,
+            status="active",
+            category__id=self.kwargs['category_id']
+        ).order_by('-published_at')
+        return query    
